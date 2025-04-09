b783cd8d655cde9bbb9d02d0ed4118543fef104f
JemLBT <adesignjem@gmail.com>
2025-04-09 17:21:19 +0100

    search working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+      </div>
     </div>
   );
 }
